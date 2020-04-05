f98ef5a1897325cb2b1ae29ac305bdea2a04e662
zenjava <138962816@qq.com>
2020-04-05 11:47:53 +0800

    删除：   * interface文件夹。

--- packages/ramada/interface/ramada.flow.js
+++
@@ -1,5 +0,0 @@
-declare module '@lugia/ramada' {
-  declare export function always(val: any): () => any;
-
-  declare export var alwaysEmptyString: Function;
-}
--- packages/theme-config/interface/theme-config.flow.js
+++
@@ -1,9 +0,0 @@
-import type { ThemeConfig } from '@lugia/theme-core';
-declare module '@lugia/theme-config' {
-  declare export type ThemeConfigProps = {
-    children: React.Node,
-    config: { [key: string]: ThemeConfig },
-    className?: string,
-  };
-  declare export default React.ComponentType<ThemeConfigProps>;
-}
