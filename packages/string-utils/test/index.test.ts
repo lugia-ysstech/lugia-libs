a8e91af51b4678f31415da0b89167fd77cb313be
likunru <likunru@ysstech.com>
2021-03-05 16:38:36 +0800

    add: string-utils

--- packages/string-utils/test/index.test.ts
+++ packages/string-utils/test/index.test.ts
@@ -7,6 +7,9 @@ describe('string-utils', () => {
     expect(enDash2UpperCaseLetter('a')).toEqual('A');
     expect(enDash2UpperCaseLetter('a-b-c')).toEqual('ABC');
     expect(enDash2UpperCaseLetter('a-bc')).toEqual('ABc');
+    expect(enDash2UpperCaseLetter('amount-input')).toEqual('AmountInput');
+    expect(enDash2UpperCaseLetter('button')).toEqual('Button');
+    expect(enDash2UpperCaseLetter('tree-select')).toEqual('TreeSelect');
   });
 
   it('upperCaseLetter2EnDash', () => {
@@ -16,5 +19,8 @@ describe('string-utils', () => {
     expect(upperCaseLetter2EnDash('AB')).toEqual('a-b');
     expect(upperCaseLetter2EnDash('aB2')).toEqual('a-b2');
     expect(upperCaseLetter2EnDash('aB2C')).toEqual('a-b2-c');
+    expect(upperCaseLetter2EnDash('AmountInput')).toEqual('amount-input');
+    expect(upperCaseLetter2EnDash('Button')).toEqual('button');
+    expect(upperCaseLetter2EnDash('TreeSelect')).toEqual('tree-select');
   });
 });
